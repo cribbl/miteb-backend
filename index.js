95eb288ed90f484aadd86ddcc67d31b72d4423c3
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-05-06 15:54:53 +0530

    index change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 app = express()
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
       res.send('Hello world!');
 });
 
-app.listen(3002);
-console.log('running');
+app.listen(app.get('port'), function() {
+  console.log('running');
+});
