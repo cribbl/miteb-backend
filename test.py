56a5584036f32d9b38c523c1bd32e9af3081b4cd
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-05-10 13:50:32 +0530

    temp signup

--- test.py
+++ test.py
@@ -29,21 +29,21 @@ update_user_params = {
 }
 
 import urllib
-import urllib3
+import urllib2
 
 
-# params = urllib.urlencode(update_user_params)
-# res = urllib2.urlopen("http://localhost:9000/signup?username=admin&password=pas")
-# print res.read()
+params = urllib.urlencode(update_user_params)
+res = urllib2.urlopen("https://miteventbooking.herokuapp.com/login?username=admin&password=pass")
+print res.read()
 
-import json
-encoded_body = json.dumps({
-        "username": "admin",
-        "password": "password",
-    })
+# import json
+# encoded_body = json.dumps({
+#         "username": "admin",
+#         "password": "password",
+#     })
 
-http = urllib3.PoolManager()
+# http = urllib3.PoolManager()
 
-r = http.request('POST', 'http://localhost:9000/signup', body=encoded_body)
+# r = http.request('POST', 'http://localhost:9000/signup', body=encoded_body)
 
-print r.read() #
+# print r.read() #
