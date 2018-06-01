880a14549ba895f3bc1654b02e25da710b790103
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-06-01 20:23:42 +0530

    generate pdf and upload to s3

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const fs = require('fs')
 var cors = require('cors')
 var pdf = require('html-pdf');
 var ejs = require('ejs');
-const base_url = "dev-miteventbooking.herokuapp.com";
-
-// fetchRooms = require('./models/fetchRooms');
 
 var serviceAccount = require("./config.json");
 
@@ -28,7 +25,6 @@ AWS.config.update({
     accessKeyId: "***REMOVED***",
     secretAccessKey: "***REMOVED***",
 });
-
 const s3 = new AWS.S3();
 const bucketName = 'miteb'
 
@@ -134,30 +130,6 @@ app.get('/update-user', function(req, res) {
     });
 })
 
-var uploadToS3 = function(filename, callback) {
-  fs.readFile(filename, function(err, data) {
-    if(err) {
-      console.log("error while reading", err)
-      callback(err)
-    }
-    else {
-      let params = {Bucket: bucketName, Key: filename, Body: data}
-      s3.putObject(params, function(err, data) {
-        if(err) {
-          console.log("error" + err)
-          callback(err)
-          return
-        }
-        else {
-          console.log("uploaded succcessfully")
-          downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`;
-          callback(null, downloadURL)
-          return
-        }
-      })
-    }
-  })
-}
 
 app.get('/send-otp', function(req,res) {
   var userID = req.query.userID;
@@ -192,12 +164,35 @@ app.get('/send-otp', function(req,res) {
   });
 });
 
+var uploadToS3 = function(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if(err) {
+      console.log("error while reading", err)
+      callback(err)
+    }
+    else {
+      let params = {Bucket: bucketName, Key: filename, Body: data}
+      s3.putObject(params, function(err, data) {
+        if(err) {
+          console.log("error" + err)
+          callback(err)
+          return
+        }
+        else {
+          console.log("uploaded succcessfully")
+          downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`;
+          callback(null, downloadURL)
+          return
+        }
+      })
+    }
+  })
+}
+
 app.get('/generate-pdf', function(req,res){
   var eventID = req.query.eventID;
   var filename = `${eventID}.pdf`
 
-  // var eventid = "-LD8H1FvjaD6m-qHdr_P";
-  // var eventid = "-LDIKlEC2c-EkzhX_RpK";
   var eventref = admin.database().ref('events/' + eventID);
   eventref.on("value", function(snapshot) {
     var html;
