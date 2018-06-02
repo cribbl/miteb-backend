d17f01130da3452731560ea45aaa76266d44d8b3
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-06-02 20:10:16 +0530

    added routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 var auth_controller = require('../controllers/authController.js');
+var user_controller = require('../controllers/userController.js');
+var post_controller = require('../controllers/postController.js');
 
-router.get('/', auth_controller.index);
+router.get('/',user_controller.index);
+
+router.get('/update-user', user_controller.update_user);
 
 router.get('/send-otp', auth_controller.send_otp);
+
+router.get('/generate-pdf', auth_controller.generate_pdf);
+
+router.post('/send-notif', post_controller.send_notif);
+
+router.post('/send-email', post_controller.send_email);
 
 module.exports = router;
