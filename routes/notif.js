b6ed6cb2f704548c40ee0e573f85104388f8092a
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-08-17 16:58:54 +0530

    added complaint template

--- routes/notif.js
+++ routes/notif.js
@@ -5,9 +5,11 @@ var notif_controller = require('../controllers/notifController');
 
 router.post('/sendOTP', notif_controller.send_otp);
 
-router.get('/send-email', notif_controller.send_email);
+// router.get('/send-email', notif_controller.send_email);
 
 router.get('/send-email-template', notif_controller.sendEventBookingStatusEmailTemplate);
+
+router.get('/send-complaint-email', notif_controller.sendComplaintEmailTemplate);
 
 router.post('/send-push', notif_controller.send_push);
 
