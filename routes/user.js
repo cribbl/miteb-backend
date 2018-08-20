dd1b5f85a98fdc9936d18365b907048c2bb5f245
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-08-20 15:30:02 +0530

    custom-signup

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,8 @@ router.post('/signup-club', user_controller.signup_club);
 
 router.post('/signup-fa', user_controller.signup_fa);
 
+router.get('/signup-custom', user_controller.custom_signup);
+
 router.get('/update-user', user_controller.update_user);
 
 
