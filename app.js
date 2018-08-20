fe0f6f4a4dd4761cf0e182d80e7f277db527f531
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-08-20 20:29:52 +0530

    request auth

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 require('./config/config.js');
 app = express();
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://staging.cribblservices.com/*');
+    res.header('Access-Control-Allow-Origin', 'https://staging.cribblservices.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
