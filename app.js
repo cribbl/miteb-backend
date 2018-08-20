c7f316db89d869814e8cd8d2dccb8e634fb1a6c1
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-08-20 20:36:31 +0530

    request auth

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
