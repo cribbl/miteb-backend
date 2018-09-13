f7680d04399f0e067154696233bced2202cd1f86
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-13 11:56:52 +0530

    style: Add standardjs as dependency. Run linter

--- config/config.js
+++ config/config.js
@@ -1,41 +1,37 @@
 const AWS = require('aws-sdk')
 const fs = require('fs')
 AWS.config.update({
-    accessKeyId: "AKIAIWXHQYTV6HV7KMKA",
-    secretAccessKey: "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",
-});
+  accessKeyId: 'AKIAIWXHQYTV6HV7KMKA',
+  secretAccessKey: '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'
+})
 
-const admin = require('firebase-admin');
-var serviceAccount = process.env.NODE_ENV == 'production' ? require("./../config.json") : require("./../config-development.json");
+const admin = require('firebase-admin')
+var serviceAccount = process.env.NODE_ENV == 'production' ? require('./../config.json') : require('./../config-development.json')
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.NODE_ENV == 'production' ? "https://staging-mit-event-booking.firebaseio.com" : "https://mit-clubs-management.firebaseio.com"
-});
+  databaseURL: process.env.NODE_ENV == 'production' ? 'https://staging-mit-event-booking.firebaseio.com' : 'https://mit-clubs-management.firebaseio.com'
+})
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3()
 const bucketName = 'miteb'
 
-exports.uploadToS3 = function(filename, callback) {
-  fs.readFile(filename, function(err, data) {
-    if(err) {
-      console.log("error while reading", err)
+exports.uploadToS3 = function (filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      console.log('error while reading', err)
       callback(err)
-    }
-    else {
-      let params = {Bucket: bucketName, Key: filename, Body: data}
-      s3.putObject(params, function(err, data) {
-        if(err) {
-          console.log("error" + err)
+    } else {
+      let params = { Bucket: bucketName, Key: filename, Body: data }
+      s3.putObject(params, function (err, data) {
+        if (err) {
+          console.log('error' + err)
           callback(err)
-          return
-        }
-        else {
-          console.log("uploaded succcessfully")
-          downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`;
+        } else {
+          console.log('uploaded succcessfully')
+          downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`
           callback(null, downloadURL)
-          return
         }
       })
     }
   })
-};
+}
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var user_controller = require('../controllers/userController');
+var user_controller = require('../controllers/userController')
 
-router.post('/signup-club', user_controller.signup_club);
+router.post('/signup-club', user_controller.signup_club)
 
-router.post('/signup-fa', user_controller.signup_fa);
+router.post('/signup-fa', user_controller.signup_fa)
 
-router.get('/signup-custom', user_controller.custom_signup);
+router.get('/signup-custom', user_controller.custom_signup)
 
-router.get('/update-user', user_controller.update_user);
+router.get('/update-user', user_controller.update_user)
 
-
-module.exports = router;
+module.exports = router
