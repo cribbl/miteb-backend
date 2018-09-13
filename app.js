adc5649498a299a49af1b9c249f16ad149997592
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-13 12:19:14 +0530

    refactor: Use path.join() to instead of +

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 require('./config/config.js')
 const app = express()
 var allowCrossDomain = function (req, res, next) {
@@ -22,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(allowCrossDomain)
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(function (req, res, next) {
   // console.log(req.headers)
   if (process.env.NODE_ENV == 'production' && (req.headers.origin != 'https://prod.cribblservices.com')) {
