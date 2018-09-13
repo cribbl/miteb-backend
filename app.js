b637deadd3239c233510e0cb3342587d7260375b
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-13 11:56:52 +0530

    style: Add standardjs as dependency. Run linter

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
-require('./config/config.js');
-app = express();
-var allowCrossDomain = function(req, res, next) {
-	var devAllowedOrigins = ['http://localhost:3000', 'https://staging.cribblservices.com'];
-	var prodAllowedOrigins = ['https://prod.cribblservices.com'];
+require('./config/config.js')
+const app = express()
+var allowCrossDomain = function (req, res, next) {
+  var devAllowedOrigins = ['http://localhost:3000', 'https://staging.cribblservices.com']
+  var prodAllowedOrigins = ['https://prod.cribblservices.com']
 
-	var allowedOrigins = process.env.NODE_ENV == 'production' ? prodAllowedOrigins : devAllowedOrigins;
-    var origin = req.headers.origin;
-	if(allowedOrigins.indexOf(origin) > -1){
-	   res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  var allowedOrigins = process.env.NODE_ENV === 'production' ? prodAllowedOrigins : devAllowedOrigins
+  var origin = req.headers.origin
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
 
-    next();
+  next()
 }
 
 // app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(allowCrossDomain);
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next) {
-	// console.log(req.headers)
-	if(process.env.NODE_ENV == 'production' && (req.headers.origin != 'https://prod.cribblservices.com')) {
-		res.status(503).send("Unauthorized");
-		return;
-	}
-	else if(process.env.NODE_ENV == 'development' && (req.headers.origin != 'https://staging.cribblservices.com')) {
-		res.status(503).send("Unauthorized");
-		return;
-	}
-	else{
-		next();
-	}
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(allowCrossDomain)
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use(function (req, res, next) {
+  // console.log(req.headers)
+  if (process.env.NODE_ENV == 'production' && (req.headers.origin != 'https://prod.cribblservices.com')) {
+    res.status(503).send('Unauthorized')
+  } else if (process.env.NODE_ENV == 'development' && (req.headers.origin != 'https://staging.cribblservices.com')) {
+    res.status(503).send('Unauthorized')
+  } else {
+    next()
+  }
 })
 
-app.get('/', function(req, res) {
-	res.send(`This is the ${app.settings.env} server`);
-	// res.render('test')
+app.get('/', function (req, res) {
+  res.send(`This is the ${app.settings.env} server`)
+  // res.render('test')
 })
 
-app.get('/', function(req, res) {
-	res.render('test')
+app.get('/', function (req, res) {
+  res.render('test')
 })
 
 var userRoutes = 	require('./routes/user')
@@ -53,12 +49,11 @@ var eventRoutes = require('./routes/event')
 var notifRoutes = require('./routes/notif')
 var complaintsRoutes = require('./routes/complaints')
 
-app.use('/user', userRoutes);
-app.use('/event', eventRoutes);
-app.use('/notif', notifRoutes);
-app.use('/complaint', complaintsRoutes);
+app.use('/user', userRoutes)
+app.use('/event', eventRoutes)
+app.use('/notif', notifRoutes)
+app.use('/complaint', complaintsRoutes)
 
-
-app.listen(process.env.PORT || 9000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 9000, function () {
+  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env)
+})
