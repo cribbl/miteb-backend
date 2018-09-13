812e24015385f1243679f261bfd856e5f5a9d978
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-13 13:52:49 +0530

    style: Multiple style fixes across files

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,20 +1,19 @@
 const admin = require('firebase-admin')
+const path = require('path')
 var Excel = require('exceljs')
 const moment = require('moment')
 
-var config = require('../config/config.js')
+function snapshotToArray (snapshot) {
+  var returnArr = []
+  snapshot.forEach(function (childSnapshot) {
+    var item = childSnapshot.key
+    returnArr.push(item)
+  })
+  return returnArr
+};
 
 exports.generate_sheet = function (req, res) {
   try {
-    function snapshotToArray (snapshot) {
-	    var returnArr = []
-	    snapshot.forEach(function (childSnapshot) {
-	    	var item = childSnapshot.key
-	    	returnArr.push(item)
-	    })
-	    return returnArr
-	 	};
-
     var months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     var type = req.query.mode
     var dated
@@ -22,7 +21,7 @@ exports.generate_sheet = function (req, res) {
     var status
     var workbook = new Excel.Workbook()
     var complaintRef = admin.database().ref()
-    if (type == 'CUSTOM') {
+    if (type === 'CUSTOM') {
       var d1 = req.query.from
       var d2 = req.query.to
       var worksheet = workbook.addWorksheet('Complaints')
@@ -34,7 +33,7 @@ exports.generate_sheet = function (req, res) {
       var complaintID
       complaintRef.child('complaints/').once('value', function (snapshot) {
         complaintID = snapshotToArray(snapshot)
-        complaintCount = complaintID.length
+        let complaintCount = complaintID.length
         var i = 0
         complaintID.forEach(function (element) {
           complaintRef.child('complaints/' + element).once('value', function (snapshot) {
@@ -46,31 +45,30 @@ exports.generate_sheet = function (req, res) {
               subject = snapshot.child('subject').val()
               status = snapshot.child('isResolved').val()
               dated = t1.format('dddd, Do MMM YYYY')
-              status = (status == true) ? 'Resolved' : 'Pending'
+              status = (status === true) ? 'Resolved' : 'Pending'
               worksheet.addRow({ subject: subject, dated: dated, status: status })
             }
             i += 1
-            if (i == complaintCount) {
-              workbook.xlsx.writeFile(__dirname + '/Complaints.xlsx').then(function () {
-	              	console.log('file is written')
-	              	res.download(__dirname + '/Complaints.xlsx', function (err, result) {
-	                  if (err) {
-	                  	console.log('Error downloading file: ' + err)
-	                  } else {
-	                    	console.log('File downloaded successfully')
-	                  }
-	              	})
-	            	})
+            if (i === complaintCount) {
+              workbook.xlsx.writeFile(path.join(__dirname, '/Complaints.xlsx')).then(function () {
+                console.log('file is written')
+                res.download(path.join(__dirname, '/Complaints.xlsx'), function (err, result) {
+                  if (err) {
+                    console.log('Error downloading file: ' + err)
+                  } else {
+                    console.log('File downloaded successfully')
+                  }
+                })
+              })
             }
           })
         })
       })
-    } else if (type == 'ALL') {
+    } else if (type === 'ALL') {
       console.log('extract monthly')
-      var complaintID
       complaintRef.child('complaints/').once('value', function (snapshot) {
         complaintID = snapshotToArray(snapshot)
-        complaintCount = complaintID.length
+        let complaintCount = complaintID.length
         var i = 0
         complaintID.forEach(function (element) {
           complaintRef.child('complaints/' + element).once('value', function (snapshot) {
@@ -78,31 +76,31 @@ exports.generate_sheet = function (req, res) {
             var t1 = moment(dated, 'DD-MM-YYYY')
             var mon = t1.month()
             if (workbook.getWorksheet(months[mon])) {
-			                	worksheet = workbook.getWorksheet(months[mon])
-			                } else {
-			                 	var worksheet = workbook.addWorksheet(months[mon])
-			                	worksheet.columns = [
+              // let worksheet = workbook.getWorksheet(months[mon])
+            } else {
+              var worksheet = workbook.addWorksheet(months[mon])
+              worksheet.columns = [
                 { header: 'Subject', key: 'subject', width: 40 },
                 { header: 'Date of complaint', key: 'dated', width: 30 },
                 { header: 'Status', key: 'status', width: 20 }
               ]
-			                }
-			                subject = snapshot.child('subject').val()
+            }
+            subject = snapshot.child('subject').val()
             status = snapshot.child('isResolved').val()
             dated = t1.format('dddd, Do MMM YYYY')
-            status = (status == true) ? 'Resolved' : 'Pending'
+            status = (status === true) ? 'Resolved' : 'Pending'
             worksheet.addRow({ subject: subject, dated: dated, status: status })
             i += 1
-            if (i == complaintCount) {
-              workbook.xlsx.writeFile(__dirname + '/Complaints.xlsx').then(function () {
-	              	console.log('file is written')
-                  	res.download(__dirname + '/Complaints.xlsx', function (err, result) {
-	                    if (err) {
-	                    	console.log('Error downloading file: ' + err)
-	                    } else {
-			              	console.log('File downloaded successfully')
-		                }
-	              	})
+            if (i === complaintCount) {
+              workbook.xlsx.writeFile(path.join(__dirname, '/Complaints.xlsx')).then(function () {
+                console.log('file is written')
+                res.download(path.join(__dirname, '/Complaints.xlsx'), function (err, result) {
+                  if (err) {
+                    console.log('Error downloading file: ' + err)
+                  } else {
+                    console.log('File downloaded successfully')
+                  }
+                })
               })
             }
           })
