29fec1d66c8bbe12d0baf6a73ac6050b75cd4369
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-13 11:56:52 +0530

    style: Add standardjs as dependency. Run linter

--- routes/notif.js
+++ routes/notif.js
@@ -1,28 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var notif_controller = require('../controllers/notifController');
+var notif_controller = require('../controllers/notifController')
 
-router.post('/sendOTP', notif_controller.send_otp);
+router.post('/sendOTP', notif_controller.send_otp)
 
-router.post('/send-email', notif_controller.send_email);
+router.post('/send-email', notif_controller.send_email)
 
-router.get('/send-email-template', notif_controller.sendEventBookingStatusEmailTemplate);
+router.get('/send-email-template', notif_controller.sendEventBookingStatusEmailTemplate)
 
-router.get('/send-complaint-email', notif_controller.sendComplaintEmailTemplate);
-
-router.get('/send-clubApproval-email', notif_controller.sendClubApprovalStatusEmailTemplate)
-
-router.post('/send-sms', notif_controller.send_sms);
-
-router.get('/send-email-template', notif_controller.sendEventBookingStatusEmailTemplate);
-
-router.get('/send-complaint-email', notif_controller.sendComplaintEmailTemplate);
+router.get('/send-complaint-email', notif_controller.sendComplaintEmailTemplate)
 
 router.get('/send-clubApproval-email', notif_controller.sendClubApprovalStatusEmailTemplate)
 
-router.post('/send-push', notif_controller.send_push);
+router.post('/send-sms', notif_controller.send_sms)
 
-router.get('/remind-push', notif_controller.send_push_custom);
+router.get('/send-email-template', notif_controller.sendEventBookingStatusEmailTemplate)
 
-module.exports = router;
+router.get('/send-complaint-email', notif_controller.sendComplaintEmailTemplate)
+
+router.get('/send-clubApproval-email', notif_controller.sendClubApprovalStatusEmailTemplate)
+
+router.post('/send-push', notif_controller.send_push)
+
+router.get('/remind-push', notif_controller.send_push_custom)
+
+module.exports = router
