eb9f33d703b29518557483338ab6f19a8d1942eb
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-09-17 10:50:02 +0530

    removed unwanted files and folders

--- controllers/tt.py
+++
@@ -1,6 +0,0 @@
-import datetime
-base = datetime.datetime.today()
-date_list = [base + datetime.timedelta(days=x) for x in range(0, 60)]
-
-for i in date_list:
-	print("\"" + str(i.strftime("%d-%m-%y")) + "\" : [ ],")
