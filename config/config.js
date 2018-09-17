8b183b3eb5af4f783122e9b99fd92a01bed2b490
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-17 17:21:06 +0530

    Squashed commit of the following:
    commit eb9f33d703b29518557483338ab6f19a8d1942eb
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Mon Sep 17 10:50:02 2018 +0530
    
        removed unwanted files and folders
    
    commit 9a906d85726659376b7a911607862bddba3309ff
    Merge: 06ce4b3 812e240
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 17:18:24 2018 +0530
    
        Merge pull request #7 from cribblportal/style-fixes
    
        Style fixes
    
    commit c8338449a1b155d7ef8aa23339f75ea60f2aa6a0
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 13:52:49 2018 +0530
    
        style: Multiple style fixes across files
    
    commit 0857b881d6a50347d6b22405f4f1e3eb5fa83475
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:41:05 2018 +0530
    
        style: Styles fixes
    
    commit 1881f5de0e4d839f179bea80a34d727d30aa792e
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:30:05 2018 +0530
    
        style: Fix style errors
    
    commit da26c0cb2db4453f9686bafd32c2d8871e2ddbc7
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:24:16 2018 +0530
    
        style: Use `===` instead of `==`  and `!==`  instead of `!=`
    
    commit f823a2ef3808e50874f95dd752599111ea043ed6
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:19:14 2018 +0530
    
        refactor: Use path.join() to instead of +
    
    commit 97979f73ead4ae0989172cc4b8bb2cb82240a07a
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:15:32 2018 +0530
    
        refactor: Remove cors as it isn't being used
    
    commit 13500306ea95b1cedd857241826a468193b0ec26
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 12:13:51 2018 +0530
    
        style: Add test script to package.json that runs standard
    
    commit 676be4318381637d8f7052fbf23dd53dc332576c
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 11:56:52 2018 +0530
    
        style: Add standardjs as dependency. Run linter
    
    commit 81b8ab96b9720d564858c4922074f5390563f861
    Merge: 06ce4b3 b1bb513
    Author: Rohit Sarkar <rohitsarkar5398@gmail.com>
    Date:   Thu Sep 13 11:44:40 2018 +0530
    
        Merge branch 'master' into dev
        Merge commit. Bring dev up to date.
    
    commit 005b41bfea9fd064fa34aa66c33e08aff7244dc9
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Tue Sep 4 13:48:42 2018 +0530
    
        notif/remind-push
    
    commit 089461898993feb87d935c183a36ccf43bfc1f8d
    Merge: 9ddf312 467fa08
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sun Aug 26 14:30:49 2018 +0530
    
        handle speacial chars in uid
    
    commit d7d77300faaa956916c5c659ab9fa93a0c5adcfe
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sun Aug 26 13:18:36 2018 +0530
    
        generate uid for special characters
    
    commit 4f1db68af4df56cf52bf1532f6a8c861736fcf4d
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:57:27 2018 +0530
    
        agh, nothing
    
    commit 8d4fd37c0eca08fd5101f099f28ac1ed18ec4751
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:55:00 2018 +0530
    
        reverted last 3-4 commits
    
    commit 301281141b47e5d7507d0de8dc828086a7adffae
    Merge: af0694d e550929
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:51:23 2018 +0530
    
        conflict resolved
    
    commit 7819ab03cfe3ff50d128ec3463cec958b15d2977
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:50:08 2018 +0530
    
        allowed localhost
    
    commit 413c666fbbcc6d35c52515c902b0ec29777cbb0d
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:44:50 2018 +0530
    
        typo
    
    commit 359d2ac504f7dd8db7b1cc69f02b4aecbbb46b2d
    Author: bhawesh96 <bhansalibhawesh@yahoo.com>
    Date:   Sat Aug 25 14:37:50 2018 +0530
    
        allow localhost requests

--- config/config.js
+++ config/config.js
@@ -1,41 +1,37 @@
 const AWS = require('aws-sdk')
 const fs = require('fs')
 AWS.config.update({
-    accessKeyId: "***REMOVED***",
-    secretAccessKey: "***REMOVED***",
-});
+  accessKeyId: '***REMOVED***',
+  secretAccessKey: '***REMOVED***'
+})
 
-const admin = require('firebase-admin');
-var serviceAccount = process.env.NODE_ENV == 'production' ? require("./../config.json") : require("./../config-development.json");
+const admin = require('firebase-admin')
+var serviceAccount = process.env.NODE_ENV === 'production' ? require('./../config.json') : require('./../config-development.json')
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.NODE_ENV == 'production' ? "https://staging-mit-event-booking.firebaseio.com" : "https://mit-clubs-management.firebaseio.com"
-});
+  databaseURL: process.env.NODE_ENV === 'production' ? 'https://staging-mit-event-booking.firebaseio.com' : 'https://mit-clubs-management.firebaseio.com'
+})
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3()
 const bucketName = 'miteb'
 
-exports.uploadToS3 = function(filename, callback) {
-  fs.readFile(filename, function(err, data) {
-    if(err) {
-      console.log("error while reading", err)
+exports.uploadToS3 = function (filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      console.log('error while reading', err)
       callback(err)
-    }
-    else {
-      let params = {Bucket: bucketName, Key: filename, Body: data}
-      s3.putObject(params, function(err, data) {
-        if(err) {
-          console.log("error" + err)
+    } else {
+      let params = { Bucket: bucketName, Key: filename, Body: data }
+      s3.putObject(params, function (err, data) {
+        if (err) {
+          console.log('error' + err)
           callback(err)
-          return
-        }
-        else {
-          console.log("uploaded succcessfully")
-          downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`;
+        } else {
+          console.log('uploaded succcessfully')
+          let downloadURL = `https://s3.amazonaws.com/${bucketName}/${filename}`
           callback(null, downloadURL)
-          return
         }
       })
     }
   })
-};
+}
